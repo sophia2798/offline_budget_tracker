d80a77244bf33990b986eef5c36d50f7635a2f23
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-11-03 19:13:53 -0800

    attempt 2 to fix heroku bug, add process.env.PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -24,10 +24,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/api.js"));
-
-app.get("/", (req,res) => {
-  res.sendFile("public/index.html");
-});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
